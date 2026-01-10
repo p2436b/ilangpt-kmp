83ae31b8e58eb58060e73ce3d90d9c7b063eb512
Peyman Bayat <p2436b@gmail.com>
2026-01-10 17:21:02 +0300

    Fix some issues in ios app

--- composeApp/src/iosMain/kotlin/tr/com/ilangpt/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/tr/com/ilangpt/MainViewController.kt
@@ -1,11 +1,7 @@
 package tr.com.ilangpt
 
-import androidx.compose.runtime.remember
 import androidx.compose.ui.window.ComposeUIViewController
-import tr.com.ilangpt.data.datasource.createDataStore
 
 fun MainViewController() = ComposeUIViewController {
-  App(
-    prefs = remember { createDataStore() }
-  )
+  App()
 }
--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -3,22 +3,22 @@ import GoogleSignIn
 import ComposeApp
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-
+    
     func application(
-      _ app: UIApplication,
-      open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]
+        _ app: UIApplication,
+        open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]
     ) -> Bool {
-      var handled: Bool
-
-      handled = GIDSignIn.sharedInstance.handle(url)
-      if handled {
-        return true
-      }
-
-      // Handle other custom URL types.
-
-      // If not handled by this app, return false.
-      return false
+        var handled: Bool
+        
+        handled = GIDSignIn.sharedInstance.handle(url)
+        if handled {
+            return true
+        }
+        
+        // Handle other custom URL types.
+        
+        // If not handled by this app, return false.
+        return false
     }
 }
 
@@ -28,12 +28,12 @@ struct iOSApp: App {
         InitKoinKt.doInitKoin()
     }
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
-
+    
     var body: some Scene {
         WindowGroup {
             ContentView().onOpenURL(perform: { url in
-                            GIDSignIn.sharedInstance.handle(url)
-                        })
+                GIDSignIn.sharedInstance.handle(url)
+            })
         }
     }
 }
